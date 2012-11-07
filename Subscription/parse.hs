3697bdfd03ae3e0b66fc0c93854c4c8b8704708d
yoeight <yo.eight@gmail.com>
2012-11-07 08:54:28 +0100

    Why so specific ?

--- Subscription/parse.hs
+++ Subscription/parse.hs
@@ -1,21 +1,28 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
+
 module Subscription.Parse where
 
 import Subscription.Params
 
+import Control.Monad
+import Control.Monad.Trans.Class
 import Control.Pipe
+
 import qualified Data.ByteString as B
+import Data.Foldable
 import qualified Data.Text as T
 
-sourceParams :: Monad m =>
-                [(B.ByteString, B.ByteString)]
-                -> Pipe () (B.ByteString, B.ByteString) m ()
-sourceParams []     = return ()
-sourceParams (x:xs) = yield x >> sourceParams xs
+source :: (Monad m, Foldable f) =>
+          f a
+          -> Pipe () a m ()
+source = traverse_ yield
 
-parseParam :: Monad m =>
-              Pipe (B.ByteString, B.ByteString) SubParam  m ()
+parseParam :: Monad m => Pipe (B.ByteString, B.ByteString) SubParam  m ()
 parseParam = error "todo"
 
-hubPrefix :: Monad m =>
-             Pipe B.ByteString B.ByteString m ()
-hubPrefix = (yield . snd . B.breakSubstring (B.pack [104,117,98,46])) =<< await
+hubPrefix :: Monad m => Pipe B.ByteString Bool m ()
+hubPrefix = (yield . B.isPrefixOf (B.pack [104,117,98,46])) =<< await
+
+printer = forever ((lift . print) =<< await)
+
+test = runPipe (printer <+< hubPrefix <+< source [(B.pack [104,117,98,46,116,111,112,105,99])]) -- hub.topic
