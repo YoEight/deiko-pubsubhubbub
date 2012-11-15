eac3594ecddf3f0a9479ecd27670edc39e26525e
yoeight <yo.eight@gmail.com>
2012-11-15 08:46:31 +0100

    Get less specific on inner verification monad

--- Subscription/verification.hs
+++ Subscription/verification.hs
@@ -24,7 +24,8 @@ import Subscription.Conf
 
 data Response = Success | Failure | Pending
 
-verification :: Pipe (Either String Req) Response (ReaderT Conf IO) ()
+verification :: (MonadIO m, MonadReader Conf m, MonadError e m)
+                => Pipe (Either String Req) Response m ()
 verification = await >>= go
   where
     go (Left _)    = yield Failure
@@ -32,24 +33,32 @@ verification = await >>= go
       | isSubscription req = if isSyncMode req then sync req else async req
       | otherwise          = async req
 
-sync :: Req -> Pipe a Response (ReaderT Conf IO) ()
+sync :: (MonadIO m, MonadReader Conf m, MonadError e m)
+        => Req
+        -> Pipe a Response m ()
 sync req = do
   result <- (lift . runEitherT) $ (confirmation req) >> (saveSubscription req)
   either (yield . const Failure) (yield . const Success) result 
   
-async :: Req -> Pipe a Response (ReaderT Conf IO) ()
+async :: (MonadIO m, MonadReader Conf m, MonadError e m) =>
+         Req
+         -> Pipe a Response m ()
 async req = do
   result <- lift $ (runEitherT . saveSubscription) req
   either (yield . const Failure) (yield . const Success) result
 
 -- Database call. Will use MongoDB
-saveSubscription :: Req -> EitherT () (ReaderT Conf IO) ()
+saveSubscription :: (MonadIO m, MonadReader Conf m, MonadError e m)
+                    => Req
+                    -> EitherT () m ()
 saveSubscription req = EitherT go
   where
     go = runDbAction (return . Left . const ()) (fmap Right (createSubscription req))
 
 -- Do a GET request using action request callback
-confirmation :: Req -> EitherT () (ReaderT Conf IO) ()
+confirmation :: MonadIO m
+                => Req
+                -> EitherT () m ()
 confirmation = error "todo"
 
 createSubscription :: Req -> Action IO ()
@@ -62,8 +71,8 @@ createSubscription (Req (Callback callback) _ (Topic topic) _ optionals) =
       document = foldr go start optionals
   in insert_ "hub_subscription" document
 
-runDbAction :: (MonadIO m, MonadError e m, MonadReader Conf m) =>
-               (e -> m a)
+runDbAction :: (MonadIO m, MonadError e m, MonadReader Conf m)
+               => (e -> m a)
                -> Action IO a
                -> m a
 runDbAction handle action = do
