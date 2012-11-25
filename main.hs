1d4cdf2f8a9b41772490f24c79d6e9ebf42279f4
yoeight <yo.eight@gmail.com>
2012-11-25 22:57:18 +0100

    Simpler application process

--- main.hs
+++ main.hs
@@ -31,37 +31,32 @@ import Network.HTTP.Types
 import Network.HTTP.Types.Header
 
 import qualified Data.ByteString.Lazy.Char8 as C
-
+ 
 conf :: Conf 
 conf = Conf (DB (Host "127.0.0.1" defaultPort) "hub_deiko")
 
-application = server <~ webapp <~ pubSub
+application = server <~ pubSub
 
 main = runMachineT application
 
 server :: MonadIO m => ProcessT m Application a
 server = construct $ liftIO . run 8080 =<< await
 
-webapp :: Process (ProcessT (RWST Conf () Report (ResourceT IO)) Request ()) Application
-webapp = construct $ await >>= go
+pubSub :: Source Application
+pubSub = construct $ yield go
   where
-    go app = yield $ builder app
-    builder app req = let action = runMachineT (app <~ (construct $ yield req))
-                          result = runRWST action conf (Report Nothing Sync)
-                      in do
-                        (_, report, _) <- result -- TODO: use report
-                        return (responseLBS status200 [] "todo")
-
-pubSub :: (MonadIO m, MonadState Report m, MonadReader Conf m, MonadError e m)
-          => Source (ProcessT m Request ())
-pubSub = construct $ yield (subscription <~ (construct $ await >>= go))
-  where
-    go req = let method  = asRight $ parseMethod $ requestMethod req
-                 params  = queryString req
-                 path    = pathInfo req
-             in case (method, path) of
-               (POST, ["subscribe"]) -> traverse_ yield params
-               _ -> reportError ClientSide "unhandled request"
+    go = \req -> let method = asRight $ parseMethod $ requestMethod req
+                     params = queryString req
+                     path   = pathInfo req
+                 in case (method, path) of
+                   (POST, ["subscribe"])
+                     | isFormUrlEncoded req -> let action = runMachineT (subscription <~ source params)
+                                                   result :: ResourceT IO (Step k o (MachineT (RWST Conf () Report (ResourceT IO)) k o), Report, ())
+                                                   result = runRWST action conf (Report Nothing Sync)
+                                               in do
+                                                 (_ , report, _) <- result
+                                                 return $ responseLBS status200 [] "todo"
+                   _ -> return $ responseLBS status404 [] ""
 
 isFormUrlEncoded :: Request -> Bool
 isFormUrlEncoded = maybe False (const True) . find (== (hContentType, "application/x-www-form-urlencoded")) . requestHeaders
