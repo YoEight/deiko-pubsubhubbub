5f471655131ed11547b4c0bb9156cf20d92be770
YoEight <yo.eight@gmail.com>
2013-08-16 01:17:12 +0200

    Introduce Validation

--- Handler/Hub.hs
+++ Handler/Hub.hs
@@ -3,6 +3,24 @@ module Handler.Hub where
 
 import Import
 
+import Control.Applicative (Applicative(..))
+import Data.List.NonEmpty
+import qualified Data.Semigroup as S
+
+data Validation e a = Failure (NonEmpty e)
+                    | Success a
+
+instance Functor (Validation e) where
+  fmap f (Success a) = Success (f a)
+  fmap _ (Failure e) = Failure e
+
+instance Applicative (Validation e) where
+  pure = Success
+  Success f <*> Success a  = Success (f a)
+  Failure e <*> Failure e' = Failure (e S.<> e')
+  Failure e <*> _          = Failure e
+  _         <*> Failure e  = Failure e
+
 getHubR :: Handler Text
 getHubR = do
   modeOpt <- lookupGetParam "hub.mode"
@@ -15,10 +33,17 @@ decision "publish"     = publish
 decision x             = invalidArgs ["unknown " <> x <> " for hub.mode"]
 
 subscription :: Handler Text
-subscription = error "subscription not implemented yet"
+subscription = error "todo"
 
 unsubscription :: Handler Text
 unsubscription = error "unsubscription not implemented yet"
 
 publish :: Handler Text
 publish = error "publish not implemented yet"
+
+param :: Text -> Maybe a -> Validation Text a
+param _ (Just a) = Success a
+param key _      = Failure (nel $ key <> " is not provided")
+
+nel :: a -> NonEmpty a
+nel a = a :| []
