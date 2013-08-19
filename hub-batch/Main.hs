60ac99a51b24740c5c09e8e7b1613bbb2ae7b0dc
YoEight <yo.eight@gmail.com>
2013-08-19 23:56:56 +0200

    [hub-batch] Generate models

--- hub-batch/Main.hs
+++ hub-batch/Main.hs
@@ -1,1 +1,13 @@
+import Data.Time.Clock
+import Data.Text (Text)
+import Data.ByteString (ByteString)
+import Database.Persist
+import Database.Persist.Sqlite
+import Database.Persist.TH
+import Database.Persist.Quasi
+import Data.Typeable (Typeable)
+
+share [mkPersist sqlOnlySettings, mkMigrate "migrateAll"]
+    $(persistFileWith lowerCaseSettings "config/models")
+
 main = print "Hello Web !"
