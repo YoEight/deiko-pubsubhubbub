8b5c5e379bc77b737c03e99729c93d20db7be292
YoEight <yo.eight@gmail.com>
2013-08-21 00:12:19 +0200

    [hub-batch] Basic content disttribution

--- hub-batch/Main.hs
+++ hub-batch/Main.hs
@@ -1,3 +1,7 @@
+import Control.Monad.Trans (lift, liftIO)
+import Control.Monad.Logger (MonadLogger)
+import Control.Monad.Trans.Resource (allocate, liftResourceT)
+import Data.Conduit
 import Data.Time.Clock
 import Data.Text (Text)
 import Data.ByteString (ByteString)
@@ -6,8 +10,52 @@ import Database.Persist.Sqlite
 import Database.Persist.TH
 import Database.Persist.Quasi
 import Data.Typeable (Typeable)
+import Network.HTTP.Conduit
+import Network.HTTP.Types.Status
+import PubState
 
 share [mkPersist sqlOnlySettings, mkMigrate "migrateAll"]
     $(persistFileWith lowerCaseSettings "config/models")
 
 main = print "Hello Web !"
+
+retrieve :: IO ()
+retrieve =
+  runSqlite dbname $ do
+  runMigration migrateAll
+  toBeDelivered $$ subscribers
+  where
+    toBeDelivered = selectSource [PubStateValue ==. Fetched] []
+
+    subscribers =
+      allocate (newManager def) closeManager >>= \(_, manager) ->
+        awaitForever $ \(Entity key (Pub topic _)) ->
+          (lift $ selectFirst [PubContentIdent ==. key] []) >>= \res ->
+            let getBytes = pubContentContent . entityVal
+                bytes    = maybe (error "absurd") getBytes res
+                filters  = [SubTopic ==. topic, SubActivated ==. True]
+                src      = selectSource filters []
+                src2     = mapOutput (bytes,) src in
+            lift (src2 $$ send key manager)
+
+    send pubId manager =
+      awaitForever $ \(content, (Entity key sub)) ->
+        do req  <- liftIO $ parseUrl $ show (subCallback sub)
+           resp <- liftResourceT $ http (req { method = "POST" }) manager
+           liftResourceT (responseBody resp $$+- return ())
+           case responseStatus resp of
+             s | status200 >= s && s < status300 -> return ()
+               | otherwise ->
+                 liftIO getCurrentTime >>= \time ->
+                   let updv      = [PubFailureCount +=. 1
+                                   ,PubFailureDate =. time]
+                       selv      = [PubFailureSubId ==. key
+                                   ,PubFailurePubId ==. pubId]
+                       onExist k = update (entityKey k) updv
+                       notExist  = insert_ (PubFailure key pubId 0 time)
+                       select    = selectFirst selv []
+                       action    = maybe notExist onExist =<< select in
+                   lift action
+
+dbname :: Text
+dbname = "deiko-pubsubhubbub.sqlite3"
